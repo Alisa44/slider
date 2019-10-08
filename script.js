db036daca2030ed7dc2ac4be5bf861b122caf279
Larisa Chepurnaya <alisa444.86@gmail.com>
2019-10-08 11:09:12 +0300

    Middle Commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const nextBtn = document.querySelector('.nextBtn');
 const prevBtn = document.querySelector('.prevBtn');
 const container = document.querySelector('.images');
+let arrayPhotos = [
+  'img/img-0', 'img/img-1', 'img/img-2',  'img/img-3',  'img/img-4',  'img/img-5',  'img/img-6'
+];
 
 let counter = 0;
 
@@ -8,17 +11,19 @@ nextBtn.addEventListener('click', nextImg);
 prevBtn.addEventListener('click', prevImg);
 
 function nextImg() {
-    if (counter === 2) {
+    container.animate([{opacity:'0.1'},{opacity:'1'}], {duration:600, fill:'forwards'});
+    if (counter === arrayPhotos.length - 1) {
         counter = -1;
     }
     counter++;
-    container.style.backgroundImage = `url('img/img-${counter}.jpg')`
+    container.style.backgroundImage = `url('${arrayPhotos[counter]}.jpg')`
 }
 
 function prevImg() {
+    container.animate([{opacity:'0.1'},{opacity:'1'}], {duration:600, fill:'forwards'});
     if (counter === 0) {
-        counter = 3;
+        counter = arrayPhotos.length;
     }
     counter--;
-    container.style.backgroundImage = `url('img/img-${counter}.jpg')`
+    container.style.backgroundImage = `url('${arrayPhotos[counter]}.jpg')`
 }
